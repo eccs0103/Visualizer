6f5384476390c6d6db012f5edd89c74322b79421
eccs0103 <eccs0103@gmail.com>
2023-02-23 20:48:11 +0400

    1.0.5
    Signed-off-by: eccs0103 <eccs0103@gmail.com>

--- scripts/structures.js
+++ scripts/structures.js
@@ -1,91 +1,34 @@
-//#region Color
-/**
- * A class that represents RGB colors.
- */
-class Color {
+"use strict";
+//#region Engine
+class Engine {
 	/**
-	 * Instantiating a color via HSV colors.
-	 * @param {Number} hue The hue parameter.
-	 * @param {Number} saturation The saturation parameter.
-	 * @param {Number} value The value parameter.
-	 * @returns Color instance.
+	 * @param {() => void} handler 
 	 */
-	static viaHSV(hue, saturation, value) {
-		/**
-		 * 
-		 * @param {Number} n 
-		 * @param {Number} k 
-		 * @returns 
-		 */
-		function f(n, k = (n + hue / 60) % 6) {
-			return (value / 100) - (value / 100) * (saturation / 100) * Math.max(Math.min(k, 4 - k, 1), 0);
-		};
-		return new Color(f(5) * 255, f(3) * 255, f(1) * 255);
+	constructor(handler) {
+		const instance = this;
+		requestAnimationFrame(function callback(time) {
+			instance.#time = time;
+			handler();
+			requestAnimationFrame(callback);
+		});
 	}
-	/**
-	 * Instance of a white color.
-	 * @readonly
-	 */
-	static get white() {
-		return new Color(255, 255, 255);
+	/** @type {DOMHighResTimeStamp} */ #time;
+	/** @readonly */ get time() {
+		return this.#time;
 	}
+}
+//#endregion
+//#region Application
+class Application {
+	static #locked = true;
 	/**
-	 * Instance of a black color.
-	 * @readonly 
+	 * @param {any} exception 
 	 */
-	static get black() {
-		return new Color(0, 0, 0);
-	}
-	/**
-	 * @param {Number} red The red parameter.
-	 * @param {Number} green The green parameter.
-	 * @param {Number} blue The blue parameter.
-	 * @param {Number} transparence The transparence parameter.
-	 */
-	constructor(red, green, blue, transparence = 1) {
-		this.#red = red;
-		this.#green = green;
-		this.#blue = blue;
-		this.#transparence = transparence;
-	}
-	/** @type {Number} */ #red;
-	/**
-	 * The red property.
-	 * @readonly
-	 */
-	get red() {
-		return this.#red;
-	}
-	/** @type {Number} */ #green;
-	/**
-	 * The green property.
-	 * @readonly
-	 */
-	get green() {
-		return this.#green;
-	}
-	/** @type {Number} */ #blue;
-	/**
-	 * The blue property.
-	 * @readonly
-	 */
-	get blue() {
-		return this.#blue;
-	}
-	/** @type {Number} */ #transparence;
-	/**
-	 * The transparence property.
-	 * @readonly
-	 */
-	get transparence() {
-		return this.#transparence;
-	}
-	/**
-	 * Converting to a string rgba(red, green, blue, transparence) of the form.
-	 * @returns The result.
-	 */
-	toString() {
-		return `rgba(${this.#red}, ${this.#green}, ${this.#blue}, ${this.#transparence})`;
+	static prevent(exception) {
+		if (this.#locked) {
+			window.alert(exception instanceof Error ? exception.stack ?? `${exception.name}: ${exception.message}` : `Invalid exception type.`);
+			location.reload();
+		} else console.error(exception);
 	}
 }
 //#endregion
--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -1,88 +1,125 @@
-const inputLoader = (/** @type {HTMLInputElement} */ (document.querySelector(`input#loader`)));
-const audioPlayer = (/** @type {HTMLAudioElement} */ (document.querySelector(`audio#player`)));
-const canvas = (/** @type {HTMLCanvasElement} */ (document.querySelector(`canvas#visualizer`)));
+"use strict";
+try {
+	const inputLoader = (/** @type {HTMLInputElement} */ (document.querySelector(`input#loader`)));
+	const audioPlayer = (/** @type {HTMLAudioElement} */ (document.querySelector(`audio#player`)));
+	const canvas = (/** @type {HTMLCanvasElement} */ (document.querySelector(`canvas#visualizer`)));
+	const divInterface = (/** @type {HTMLDivElement} */ (document.querySelector(`div#interface`)));
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-window.addEventListener(`resize`, (event) => {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-});
-const context = (() => {
-	const contextTemp = canvas.getContext(`2d`);
-	if (!contextTemp) {
-		throw new ReferenceError(`Element 'contextTemp' isn't defined.`);
+	//#region Resize
+	function resize() {
+		const rect = canvas.getBoundingClientRect();
+		canvas.width = rect.width;
+		canvas.height = rect.height;
 	}
-	return contextTemp;
-})();
-
-canvas.addEventListener(`click`, (event) => {
-	if (!inputLoader.files || inputLoader.files.length == 0) {
-		inputLoader.click();
-	} else {
-		if (audioPlayer.paused) {
-			audioPlayer.play();
-		} else {
-			audioPlayer.pause();
+	resize();
+	window.addEventListener(`resize`, resize);
+	//#endregion
+	//#region Context
+	const context = (() => {
+		const contextTemp = canvas.getContext(`2d`);
+		if (!contextTemp) {
+			throw new ReferenceError(`Element 'contextTemp' isn't defined.`);
 		}
-	}
-});
-
-console.log(`https://github.com/eccs0103/Visualizer/blob/main/resources/Pandocrator%20-%20Ethernal%20(Demo).mp3?raw=true}`);
-
-inputLoader.addEventListener(`change`, (event) => {
-	if (!inputLoader.files) {
-		throw new ReferenceError(`Files list is empty.`);
-	}
-	const file = inputLoader.files[0];
-	const url = URL.createObjectURL(file);
-	audioPlayer.src = url;
-	inputLoader.disabled = true;
-	//
-	const audioContext = new AudioContext();
-	const analyser = audioContext.createAnalyser();
-	const source = audioContext.createMediaElementSource(audioPlayer);
-	analyser.fftSize = 512;
-	// console.log(analyser.fftSize, analyser.frequencyBinCount);
-	source.connect(analyser);
-	analyser.connect(audioContext.destination);
-	const frequencyData = new Uint8Array(analyser.frequencyBinCount * 0.7);
-	//
-	requestAnimationFrame(function callback(time) {
-		analyser.getByteFrequencyData(frequencyData);
-		handler(time);
-		requestAnimationFrame(callback);
+		return contextTemp;
+	})();
+	//#endregion
+	//#region Player
+	audioPlayer.addEventListener(`play`, (event) => {
+		audioPlayer.classList.toggle(`-playing`, true);
+		audioPlayer.classList.toggle(`-paused`, false);
+	});
+	audioPlayer.addEventListener(`pause`, (event) => {
+		audioPlayer.classList.toggle(`-playing`, false);
+		audioPlayer.classList.toggle(`-paused`, true);
 	});
 
-	// let max = 0;
-	const duration = 10;
-
-	/**
-	 * @param {DOMHighResTimeStamp} time 
-	 */
-	function handler(time) {
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		const [pathWidth, pathGap] = (() => {
-			const gapTemp = canvas.width / (frequencyData.length * 5 - 1);
-			const widthTemp = gapTemp * 4;
-			return [widthTemp, gapTemp];
-		})();
-		frequencyData.forEach((data, index) => {
-			const gradient = context.createLinearGradient(0, 0, 0, canvas.height);
-			const color = Color.viaHSV(index / frequencyData.length * 360 + (time % (duration * 1000)) / (duration * 1000) * 360, 100, 100).toString();
-			gradient.addColorStop(0, color);
-			gradient.addColorStop(0.5, color);
-			gradient.addColorStop(1, Color.black.toString());
-			context.fillStyle = gradient;
-			const pathX = (pathWidth + pathGap) * index;
-			const pathHeight = canvas.height * (data / 255);
-			const pathY = canvas.height - pathHeight;
-			context.fillRect(pathX, pathY, pathWidth, pathHeight);
+	divInterface.addEventListener(`click`, (event) => {
+		if (audioPlayer.src) {
+			if (audioPlayer.paused) {
+				audioPlayer.play();
+			} else {
+				audioPlayer.pause();
+			}
+		} else {
+			inputLoader.click();
+		}
+	});
+	/* const timer = 1;
+	divInterface.addEventListener(`pointerdown`, (event) => {
+		event.stopImmediatePropagation();
+		const interval = setInterval(() => {
+			inputLoader.click();
+			clear();
+		}, timer * 1000);
+		function clear() {
+			clearInterval(interval);
+			divInterface.removeEventListener(`pointerup`, clear);
+			divInterface.removeEventListener(`pointerleave`, clear);
+		}
+		divInterface.addEventListener(`pointerup`, clear);
+		divInterface.addEventListener(`pointerleave`, clear);
+	}); */
+	//#endregion
+	//#region Analysys
+	const audioContext = new AudioContext();
+	const analyser = audioContext.createAnalyser();
+	analyser.fftSize = 512;
+	// console.log(analyser.fftSize, analyser.frequencyBinCount);
+	audioContext.createMediaElementSource(audioPlayer).connect(analyser);;
+	analyser.connect(audioContext.destination);
+	const frequencyData = new Uint8Array(analyser.frequencyBinCount * 0.7);
+	//#region File exists
+	inputLoader.addEventListener(`change`, (event) => {
+		if (!inputLoader.files) {
+			throw new ReferenceError(`Files list is empty.`);
+		}
+		const file = inputLoader.files[0];
+		const url = URL.createObjectURL(file);
+		audioPlayer.src = url;
+		audioContext.resume();
+		//
+		requestAnimationFrame(function callback(time) {
+			analyser.getByteFrequencyData(frequencyData);
+			handler(time);
+			requestAnimationFrame(callback);
 		});
-		// const index = frequencyData.findLastIndex(value => value != 0);
-		// if (index > max) {
-		// 	max = index;
-		// 	console.log(max);
-		// }
-	}
-});
+
+		// let max = 0;
+		const duration = 10;
+
+		/**
+		 * @param {DOMHighResTimeStamp} time 
+		 */
+		function handler(time) {
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			const [pathWidth, pathGap] = (() => {
+				const gapTemp = canvas.width / (frequencyData.length * 5 - 1);
+				const widthTemp = gapTemp * 4;
+				return [widthTemp, gapTemp];
+			})();
+			frequencyData.forEach((data, index) => {
+				const gradient = context.createLinearGradient(0, 0, 0, canvas.height);
+				const perentage = index / frequencyData.length;
+				const color = Color.viaHSV(perentage * 360 + (time % (duration * 1000)) / (duration * 1000) * 360, 100, 100).toString();
+				gradient.addColorStop(0, color);
+				gradient.addColorStop(audioPlayer.currentTime / audioPlayer.duration > perentage ? 2 / 3 : 1 / 3, color);
+				gradient.addColorStop(1, Color.black.toString());
+				context.fillStyle = gradient;
+				const pathX = (pathWidth + pathGap) * index;
+				const pathHeight = canvas.height * (data / 255);
+				const pathY = canvas.height - pathHeight;
+				context.fillRect(pathX, pathY, pathWidth, pathHeight);
+			});
+			// const index = frequencyData.findLastIndex(value => value != 0);
+			// if (index > max) {
+			// 	max = index;
+			// 	console.log(max);
+			// }
+		}
+	});
+	//#endregion
+	//#endregion
+	console.log(`https://github.com/eccs0103/Visualizer/blob/main/resources/Pandocrator%20-%20Ethernal%20(Demo).mp3?raw=true`);
+} catch (exception) {
+	Application.prevent(exception);
+}
